11c2a98d9d14cb2c569fc193bfa5620e34675983
ElormLokpo <benedictkomla@gmail.com>
2023-06-12 21:05:48 +0000

    'push'

--- src/farmhand/farmhand.module.ts
+++ src/farmhand/farmhand.module.ts
@@ -1,5 +1,6 @@
 import {Module, NestModule, MiddlewareConsumer} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from 'src/authentication/jwt/jwt.module';
 import { AuthMiddleware } from 'src/authentication/middleware/auth.middleware';
 import { AuthMiddlewareModule } from 'src/authentication/middleware/auth.middleware.module';
 import { FarmHandController } from './farmhand.controller';
@@ -9,6 +10,7 @@ import { FarmHandServices } from './farmhand.services';
 @Module({
     imports:[
         MongooseModule.forFeature([{name: 'FarmHandModel', schema: FarmHandSchema}]),
+        JwtModule,
         AuthMiddlewareModule
     ],
     controllers: [FarmHandController],
--- src/inventory/equipment/equipment.module.ts
+++ src/inventory/equipment/equipment.module.ts
@@ -1,5 +1,6 @@
 import {Module, NestModule, MiddlewareConsumer} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from 'src/authentication/jwt/jwt.module';
 import { AuthMiddleware } from 'src/authentication/middleware/auth.middleware';
 import { AuthMiddlewareModule } from 'src/authentication/middleware/auth.middleware.module';
 import { EquipmentController } from './equipment.controller';
@@ -10,6 +11,7 @@ import { EquipmentServices } from './equipment.services';
 @Module({
     imports:[
         MongooseModule.forFeature([{name: 'EquipmentModel', schema: EquipmentSchema}]),
+        JwtModule,
         AuthMiddlewareModule
     ],
     controllers: [EquipmentController],
